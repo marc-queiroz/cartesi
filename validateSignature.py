ab1859130e856fb2198c55de5d92e6d022e8bfa7
Marc Queiroz <marc.queiroz@gmail.com>
2022-09-10 18:38:15 -0300

    Adding PK to code (hardcoded)

--- validateSignature.py
+++ validateSignature.py
@@ -4,7 +4,7 @@ import base64
 
 signature = sys.argv[1]
 message = sys.argv[2]
-pk_pem = open('./publicKey.pem').read()
+pk_pem = '-----BEGIN PUBLIC KEY-----\nMFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAE55/U4WPfQjltCrco5ndvJb6iZdkCvmM/\nAcIcn1i9j6EGN6Y5FLqo/TnNei5YoUxlxVgxBSF8aSS1tPJ/HRL18g==\n-----END PUBLIC KEY-----'
 vk = ecdsa.VerifyingKey.from_pem(pk_pem)
 result = False
 try:
